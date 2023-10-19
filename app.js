a1a8e78868fb6be8cb3840c9241518f44fff9c9f
Sandywd433 <148445683+Sandywd433@users.noreply.github.com>
2023-10-19 05:12:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_eo2byv8", "template_81uudbt", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
